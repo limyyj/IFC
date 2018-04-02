f629d5d35a008b0023f29c204aa7dd89dfb453ab
Lim Yan Yee Joie <a0115775@nus.edu.sg>
2018-04-02 14:49:36 +0800

    Edit ifc wall

--- src/typescript/generate/models_ifc.ts
+++ src/typescript/generate/models_ifc.ts
@@ -3,17 +3,18 @@ import * as threex from "../libs/threex/threex";
 
 export function genModelTest1(): gs.IModel {
     const m: gs.IModel = new gs.Model();
+    const b: gs.IEntAttrib = m.addEntAttrib("Element_Type", gs.EGeomType.objs, gs.EDataType.type_str);
     const points: gs.IPoint[] = m.getGeom().addPoints([
             [0,0,0],    // 0
-            [1000,0,0],   // 1
-            [1000,1000,0],  // 2
+            [100,0,0],   // 1
+            [100,1000,0],  // 2
             [0,1000,0],   // 3
             [0,0,1000],   // 4
-            [1000,0,1000],  // 5
-            [1000,1000,1000], // 6
+            [100,0,1000],  // 5
+            [100,1000,1000], // 6
             [0,1000,1000],  // 7
         ]);
-    m.getGeom().addPolymesh([
+    const wall: gs.IObj = m.getGeom().addPolymesh([
             [points[0],points[1],points[2],points[3]],
             [points[0],points[4],points[5],points[1]],
             [points[1],points[5],points[6],points[2]],
@@ -21,5 +22,6 @@ export function genModelTest1(): gs.IModel {
             [points[3],points[7],points[4],points[0]],
             [points[7],points[4],points[5],points[6]],
         ]);
+    wall.setAttribValue(b, "Wall")
     return m;
 }
--- src/typescript/ifc.ts
+++ src/typescript/ifc.ts
@@ -1,4 +1,5 @@
 import * as gs from "gs-json";
+import * as threex from "./libs/threex/threex"
 
 const header: string =
 `ISO-10303-21;
@@ -38,6 +39,7 @@ const owner: string =
 const proj_rep: string =
 `#201= IFCGEOMETRICREPRESENTATIONCONTEXT($,'Model',3,1.0E-5,#210,$);
 #202= IFCGEOMETRICREPRESENTATIONSUBCONTEXT('Body','Model',*,*,*,*,#201,$,.MODEL_VIEW.,$);
+#203= IFCGEOMETRICREPRESENTATIONSUBCONTEXT('Axis','Model',*,*,*,*,#201,$,.MODEL_VIEW.,$);
 #210= IFCAXIS2PLACEMENT3D(#901,$,$);
 
 `;
@@ -69,93 +71,209 @@ const WCS: string=
 #907= IFCDIRECTION((0.,0.,-1.));
 
 `;
+export function createObj(obj: gs.IObj, nums: number[]): string {
+    let file: string = "";
 
-export function gsjson2ifc(model: gs.IModel): string {
-    let file: string = header + "DATA;\n" + project + owner + proj_rep + units + building + WCS;
-    let pointnum: number = 1000;
-    let wirenum: number = 2000;
-    let facenum: number = 3000;
-    let objnum: number = 4000;
-    let assignnum: number = 10000;
-    const objcount: number[] = [];
-    const eattribs: gs.IEntAttrib[] = model.getAllEntAttribs();
-    const tattribs: gs.ITopoAttrib[] = model.getAllTopoAttribs();
-    const groups: gs.IGroup[] = model.getAllGroups();
-    const objs: gs.IObj[] = model.getGeom().getAllObjs();
-    for (const obj of objs) {
-        const facecount: number[] = [];
-        // Get points for each face
-        const points: gs.IPoint[][][] = obj.getPoints();
-        const f_points: gs.IPoint[][] = points[1];
-        const f_positions: gs.XYZ[][] = f_points.map((f) => f.map((p) => p.getPosition()));
-        for (const facepoints of f_positions) {
-            const pointcount: number[] = []; // # no. counter
-            
+    const facecount: number[] = [];
+    // Get points for each face
+    const points: gs.IPoint[][][] = obj.getPoints();
+    const f_points: gs.IPoint[][] = points[1];
+    const f_positions: gs.XYZ[][] = f_points.map((f) => f.map((p) => p.getPosition()));
+    for (const facepoints of f_positions) {
+        const pointcount: number[] = []; // # no. counter
 
-            // Add a point to IFC file, keep track of the # no. of the points
-            for (const point of facepoints) {
-                file += "#" + pointnum + "= IFCCARTESIANPOINT" + "((" + point[0] +".," + point[1] + ".," + point[2] + ".));\n";
-                pointcount.push(pointnum);
-                pointnum++;
-            }
-            // Add a polyline to IFC file, using # no. from above
-            file += "#" + wirenum + "= IFCPOLYLOOP" + "((";
-            for (let p = 0; p < pointcount.length; p++) {
-                if (p !== pointcount.length - 1) {
-                    file += "#" + pointcount[p] + ",";
-                } else {
-                    file += "#" + pointcount[p];
-                }
-            }
-            file += "));\n";
-
-            // Add face from polyline, keep track of # no. of the faces
-            file += "#" + (wirenum + 1) + "= IFCFACEOUTERBOUND" + "(#" + wirenum + ",.T.);\n";
-            file += "#" + facenum + "= IFCFACE" + "((#" + (wirenum + 1) + "));\n\n";
-            facecount.push(facenum);
-
-            wirenum += 2;
-            facenum ++;
-         }
-
-        // Add brep to IFC file, using # no. from above (facecount), keep track of # no. (counter)
-        file += "#" + objnum + "= IFCCLOSEDSHELL ((";
-        objnum ++;
-        for (let f = 0; f < facecount.length; f++) {
-            if (f !== facecount.length - 1) {
-                file += "#" + facecount[f] + ",";
+        // Add a point to IFC file, keep track of the # no. of the points
+        for (const point of facepoints) {
+            file += "#" + nums[0] + "= IFCCARTESIANPOINT" + "((" + point[0] +".," + point[1] + ".," + point[2] + ".));\n";
+            pointcount.push(nums[0]);
+            nums[0]++;
+        }
+        // Add a polyline to IFC file, using # no. from above
+        file += "#" + nums[1] + "= IFCPOLYLOOP" + "((";
+        for (let p = 0; p < pointcount.length; p++) {
+            if (p !== pointcount.length - 1) {
+                file += "#" + pointcount[p] + ",";
             } else {
-                file += "#" + facecount[f];
+                file += "#" + pointcount[p];
             }
         }
         file += "));\n";
-        file += "#" + objnum + "= IFCFACETEDBREP (#" + (objnum-1) +");\n";
-        objnum ++;
-        file += "#" + objnum + "= IFCSHAPEREPRESENTATION(#202,'Body','Brep',(#" + (objnum-1) + "));\n";
-        objnum ++;
-        file += "#" + objnum + "= IFCPRODUCTDEFINITIONSHAPE($,$,(#" + (objnum-1) + "));\n";
-        let counter = objnum;
-        objnum ++;
 
-        // Add proxy element to IFC file, using # no. from above (counter), keep track of # no. (objcount)
-        file += "#" + objnum + "= IFCAXIS2PLACEMENT3D(#901,$,$);\n";
-        objnum ++;
-        file += "#" + objnum + "= IFCLOCALPLACEMENT(#511,#" + (objnum-1) + ");\n";
-        objnum ++;
-        file += "#" + objnum + "= IFCBUILDINGELEMENTPROXY('obj_" + objnum + "',$,'obj_" + objnum
-        + "','obj_" + objnum + "',$,#"+ objnum + ",#" + counter + ",$,$);\n\n";
-        objcount.push(objnum);
-        objnum ++;
+        // Add face from polyline, keep track of # no. of the faces
+        file += "#" + (nums[1] + 1) + "= IFCFACEOUTERBOUND" + "(#" + nums[1] + ",.T.);\n";
+        file += "#" + nums[2] + "= IFCFACE" + "((#" + (nums[1] + 1) + "));\n\n";
+        facecount.push(nums[2]);
 
-        // const value = obj.getAttribValue(eattribs[0]);
+        nums[1] += 2;
+        nums[2] ++;
+     }
+
+    // Add brep to IFC file, using # no. from above (facecount), keep track of # no. (counter)
+    file += "#" + nums[3] + "= IFCCLOSEDSHELL ((";
+    nums[3] ++;
+    for (let f = 0; f < facecount.length; f++) {
+        if (f !== facecount.length - 1) {
+            file += "#" + facecount[f] + ",";
+        } else {
+            file += "#" + facecount[f];
+        }
+    }
+    file += "));\n";
+    file += "#" + nums[3] + "= IFCFACETEDBREP (#" + (nums[3]-1) +");\n";
+    nums[3] ++;
+    file += "#" + nums[3] + "= IFCSHAPEREPRESENTATION(#202,'Body','Brep',(#" + (nums[3]-1) + "));\n";
+    nums[3] ++;
+    file += "#" + nums[3] + "= IFCPRODUCTDEFINITIONSHAPE($,$,(#" + (nums[3]-1) + "));\n";
+    let counter = nums[3];
+    nums[3] ++;
+
+    // Add proxy element to IFC file, using # no. from above (counter), keep track of # no. (objcount)
+    file += "#" + nums[3] + "= IFCAXIS2PLACEMENT3D(#901,$,$);\n";
+    nums[3] ++;
+    file += "#" + nums[3] + "= IFCLOCALPLACEMENT(#511,#" + (nums[3]-1) + ");\n";
+    nums[3] ++;
+    file += "#" + nums[3] + "= IFCBUILDINGELEMENTPROXY('obj_" + nums[3] + "',$,'obj_" + nums[3]
+    + "','obj_" + nums[3] + "',$,#"+ nums[3] + ",#" + counter + ",$,$);\n\n";
+    
+    return file;
+}
+
+export function createWall(obj: gs.IObj, nums: number[]): string {
+    let file: string = "";
+
+    // Get unique points
+    const points: gs.IPoint[] = obj.getPointsSet();
+
+    // Get lowerpts and upperpts
+    const lowerpts: gs.IPoint[] = [points[0],points[0],points[0],points[0]];
+    const upperpts: gs.IPoint[] = [points[0],points[0],points[0],points[0]];
+    for (const pt of points) {
+        if (pt.getPosition()[2] <= lowerpts[0].getPosition()[2]) {
+            lowerpts[3] = lowerpts[2];
+            lowerpts[2] = lowerpts[1];
+            lowerpts[1] = lowerpts[0];
+            lowerpts[0] = pt;
+        } else {
+            upperpts[3] = upperpts[2];
+            upperpts[2] = upperpts[1];
+            upperpts[1] = upperpts[0];
+            upperpts[0] = pt;
+        }
+    }
+    // Calc extrude_dist
+    const extrude_dist: number = upperpts[0].getPosition()[2] - lowerpts[0].getPosition()[2];
+
+    // Add points
+    const pointcount: number[] = []; // # no. counter
+    for (const pt of lowerpts) {
+        const coord: number[] = pt.getPosition();
+        file += "#" + nums[0] + "= IFCCARTESIANPOINT" + "((" + coord[0] +".," + coord[1] + ".," + coord[2] + ".));\n";
+        pointcount.push(nums[0]);
+        nums[0]++;
+    }
+
+    // Add a polyline to IFC file, using # no. from above
+    file += "#" + nums[1] + "= IFCPOLYLINE" + "((";
+    for (const p of pointcount) {
+            file += "#" + p + ",";
+    }
+    file += pointcount[0] + "));\n";
+    // Convert to area and extrude *********************************************************!! NEED LOCALPLACEMENT (extrude)
+    file += "#" + nums[2] + "= IFCARBITRARYCLOSEDPROFILEDEF(.AREA., 'face" + nums[2] + "', #" + nums[1] +");\n";
+    file += "#" + nums[3] + "= IFCEXTRUDEAREASOLID(#" + nums[2] + ",#512, #904," + extrude_dist + ");\n";
+    nums[1]++;
+    nums[2]++;
+    nums[3]++;
+    // Shape representation for Body
+    file += "#" + nums[3] + "= IFCSHAPEREPRESENTATION(#202,'Body','SweptSolid',(#" + (nums[3] - 1) + "));\n";
+    nums[3]++;
+
+    // Get pair with shortest dist
+    let shortpair: gs.IPoint[] = [lowerpts[0],lowerpts[1]];
+    for (let i = 0 ; i < lowerpts.length ; i++) {
+        for (let j = i + 1 ; j < lowerpts.length ; j++) {
+            if (threex.vectorFromPointsAtoB(lowerpts[i], lowerpts[j]).length() <= threex.vectorFromPointsAtoB(shortpair[0], shortpair[1]).length()) {
+                shortpair = [lowerpts[i], lowerpts[j]];
+            }
+        }
+    }
+    // Get other pair
+    const otherpair: gs.IPoint[] = [];
+    for (const pt of lowerpts) {
+        if ((pt !== shortpair[0]) && (pt !== shortpair[1])) {
+            otherpair.push(pt);
+        }
+    }
+
+    // Get coordinates of midpoints and construct line
+    const x1: number = (shortpair[0].getPosition()[0] + shortpair[1].getPosition()[0])/2;
+    const y1: number = (shortpair[0].getPosition()[1] + shortpair[1].getPosition()[1])/2;
+    const z1: number = (shortpair[0].getPosition()[2] + shortpair[1].getPosition()[2])/2;
+    const x2: number = (otherpair[0].getPosition()[0] + otherpair[1].getPosition()[0])/2;
+    const y2: number = (otherpair[0].getPosition()[1] + otherpair[1].getPosition()[1])/2;
+    const z2: number = (otherpair[0].getPosition()[2] + otherpair[1].getPosition()[2])/2;
+    file += "#" + nums[0] + "= IFCCARTESIANPOINT" + "((" + x1 +".," + y1 + ".," + z1 + ".));\n";
+    nums[0]++;
+    file += "#" + nums[0] + "= IFCCARTESIANPOINT" + "((" + x2 +".," + y2 + ".," + z2 + ".));\n";
+    nums[0]++;
+    file += "#" + nums[1] + "= IFCPOLYLINE" + "((" + (nums[0]-2) + "," + (nums[0]-1) + "));\n";
+    nums[1]++;
+    // Shape representation for Axis
+    file += "#" + nums[3] + "= IFCSHAPEREPRESENTATION(#203,'Axis','Curve2D',(#" + (nums[1] - 1) + "));\n";
+    nums[3]++;
+
+    // Define wall
+    file += "#" + nums[3] + "= IFCPRODUCTDEFINITIONSHAPE($,$,(#" + (nums[3]-2) + "," + (nums[3]-1) + "));\n";
+    nums[3]++;
+    file += "#" + nums[3] + "= IFCWALLSTANDARDCASE('0ZSjmOcYr8ww8iEjFQxGMf',$,'wall" + nums[3] + "',$,$,$,#" + "," + (nums[3]-1) + ",'wall" + nums[3] + "');\n\n";
+
+    return file;
+}
+
+
+
+
+
+
+
+
+export function gsjson2ifc(model: gs.IModel): string {
+    // Initialise headers and start, initialise counters
+    let file: string = header + "DATA;\n" + project + owner + proj_rep + units + building + WCS;
+    let nums: number[] = [1000, 2000, 3000, 4000, 10000]
+    const objcount: number[] = [];
+    // const eattribs: gs.IEntAttrib[] = model.getAllEntAttribs();
+    // const tattribs: gs.ITopoAttrib[] = model.getAllTopoAttribs();
+    // const groups: gs.IGroup[] = model.getAllGroups();
+    
+    // Get Element_Type attrib
+    const element_type: gs.IEntAttrib = model.getEntAttrib("Element_Type", gs.EGeomType.objs)
+    // Get all objects
+    const objs: gs.IObj[] = model.getGeom().getAllObjs();
+    
+    for (const obj of objs) {
+        if (obj.getAttribValue(element_type) == "Wall") {
+        // Generate string for creating a wall and add to file
+            let addobj: string = createWall(obj, nums);
+            file += addobj
+        } else {
+        // Generate string for creating an object and add to file
+            let addobj: string = createObj(obj, nums);
+            file += addobj;
+        }
+
+        // Update objcount and objnum
+        objcount.push(nums[3]);
+        nums[3] ++;
     }
 
     // Assign objects to building
     for (const o of objcount) {
-         file += "#" + assignnum + "=IFCRELCONTAINEDINSPATIALSTRUCTURE('assign_" + assignnum +
-         "',$,'Physical model',$,(#" + o + "),#500);\n";
-         assignnum ++;
+         file += "#" + nums[4] + "=IFCRELCONTAINEDINSPATIALSTRUCTURE('assign_" + nums[4] +
+         "',$,'Physical model',$,(#" + (nums[3]-1) + "),#500);\n";
+         nums[4] ++;
     }
+
     file += "ENDSEC;\nEND-ISO-10303-21;";
     return file;
 }
